332204fb79b3301d38b97b8f1a9376d6d2e569d3
tauhid-taaha <127302904+tauhid-taaha@users.noreply.github.com>
2025-04-28 14:23:23 +0600

    Update Login.js

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", {
+      const res = await axios.post("http://16.171.232.89:8080/api/v1/auth/login", {
         email,
         password,
       });
